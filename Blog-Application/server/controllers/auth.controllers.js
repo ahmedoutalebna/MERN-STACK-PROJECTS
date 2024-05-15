77400e15853c5fefdba350bd88f4753b980243fc
ahmedoutalebna <73033435+ahmedoutalebna@users.noreply.github.com>
2024-05-15 14:15:28 +0100

    Update auth.controllers.js
    Updating controllers authentication

--- Blog-Application/server/controllers/auth.controllers.js
+++ Blog-Application/server/controllers/auth.controllers.js
@@ -27,8 +27,6 @@ const register = async (req, res) => {
 }
 const authentication = async(req, res)=>{
     const {email, password} = req.body 
-
-    
     try
     {
         const user = await User.findOne({email})
